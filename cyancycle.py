66dd7b705bd1c104d3544476129eb309bca07515
Aaron King <aaron.k.biology@gmail.com>
2018-08-08 08:57:28 -1000

    cyancycle

--- cyancycle.py
+++ cyancycle.py
@@ -8,12 +8,13 @@ NUM_LED = 200
 print('Cyan Cycle')
 
 MY_CYCLE = colorschemesmod.TheaterChase(num_led=NUM_LED, pause_value=0.04,
-                                    num_steps_per_cycle=35, num_cycles=1)
+                                    num_steps_per_cycle=35, num_cycles=100)
 									
 MY_CYCLE2 = colorschemesmod.TheaterChase2(num_led=NUM_LED, pause_value=0.04,
-                                    num_steps_per_cycle=35, num_cycles=1)
-while True:
-				MY_CYCLE.start()
-				MY_CYCLE2.start()
+                                    num_steps_per_cycle=35, num_cycles=100)
+
+MY_CYCLE.start()
+MY_CYCLE2.start()
+
 
 print('Stopped')
