706e7baead3fd52d2a24719b20e19f382294dc6b
Aaron King <aaron.k.biology@gmail.com>
2018-08-10 15:10:14 -1000

    stuff

--- greencycle.py
+++ greencycle.py
@@ -7,7 +7,7 @@ NUM_LED = 200
 print('Visual cycle')
 
 MY_CYCLE = colorschemesmod.TheaterChase5(num_led=NUM_LED, pause_value=0.04,
-                                    num_steps_per_cycle=35, num_cycles=100)
+                                    num_steps_per_cycle=35, num_cycles=1000)
 
 while True:									
 	MY_CYCLE.start()
