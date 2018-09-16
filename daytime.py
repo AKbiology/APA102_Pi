7a2c4c672d517e4ceff185f1105c4caac62d92f8
Aaron King <aaron.k.biology@gmail.com>
2018-09-15 14:33:04 -1000

    stuff

--- daytime.py
+++ daytime.py
@@ -10,19 +10,19 @@ today7pm = now.replace(hour=19, minute=0, second=0, microsecond=0)
 
 while true:
 
-	if (now > today7am) and (now < today7pm)
+	if (now > today7am) and (now < today7pm):
 		print('White light')
 		MY_CYCLE = colorschemes.Solid(num_led=NUM_LED, pause_value=4,
                             num_steps_per_cycle=1, num_cycles=1)
 		MY_CYCLE.start()
 
-	if (now < today7am) 
+	if (now < today7am):
 		print('Five quick trips through the rainbow')
 		MY_CYCLE = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
 							num_steps_per_cycle=35, num_cycles=5)
 		MY_CYCLE.start()
 
-	if (now > today7pm)
+	if (now > today7pm):
 		print('Five quick trips through the rainbow')
 		MY_CYCLE = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
 							num_steps_per_cycle=35, num_cycles=5)
