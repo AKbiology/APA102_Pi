fb6124d41869e858d10539e56d107df882230258
Aaron King <aaron.k.biology@gmail.com>
2018-09-15 20:15:33 -1000

    stuff

--- daytime.py
+++ daytime.py
@@ -7,36 +7,44 @@ NUM_LED = 200
 now = datetime.datetime.now()
 today7am = now.replace(hour=7, minute=0, second=0, microsecond=0)
 today7pm = now.replace(hour=20, minute=0, second=0, microsecond=0)
-CRYO = 0
 
-while True:
-
-	if (now > today7am) and (now < today7pm):
-		CRYO = 1
-		print('set daytime')
-	if (now < today7am):
+if (now < today7am):
 		print('set morning')
+		CRYO = 1
+if (now > today7am) and (now < today7pm):
 		CRYO = 2
-	if (now > today7pm):
+		print('set daytime')
+if (now > today7pm):
 		print('set night')
 		CRYO = 3
 
-	if (CRYO == 1):
-		print('Daytime')
-		MY_CYCLE = colorschemes.Solid(num_led=NUM_LED, pause_value=4,
-                            num_steps_per_cycle=1, num_cycles=1)
-		MY_CYCLE.start()
+while True:
 
-
-	if (CRYO == 2):
+	if (now < today7am) and (CRYO == 1):
 		print('Early morning')
 		MY_CYCLE = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
 							num_steps_per_cycle=35, num_cycles=5)
+		CRYO = 2
 		MY_CYCLE.start()
 
+	if (now > today7am) and (now < today7pm) and (CRYO == 2):
+		print('Daytime')
+		MY_CYCLE = colorschemes.Solid(num_led=NUM_LED, pause_value=4,
+                            num_steps_per_cycle=1, num_cycles=1)
+		CRYO = 3
+		MY_CYCLE.start()
+		
 
-	if (CRYO == 3):
+	if (now > today7pm) and (CRYO == 3):
 		print('Nighttime')
 		MY_CYCLE = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
 							num_steps_per_cycle=35, num_cycles=5)
+		CRYO = 1
 		MY_CYCLE.start()
+
+	if (CRYO == 1):
+		print('it is nighttime now')
+	if (CRYO == 3):
+		print('it is daytime now')
+	if (CRYO == 2):
+		print('it is morning now')
