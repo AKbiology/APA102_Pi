a7bddfa177bf2279f0052fe6884677d7ebf0bd27
Aaron King <aaron.k.biology@gmail.com>
2018-09-15 19:07:04 -1000

    stuff

--- daytime.py
+++ daytime.py
@@ -11,21 +11,21 @@ CRYO = 1
 
 while True:
 
-	if (now > today7am) and (now < today7pm) and (CRYO = 1):
+	if (now > today7am) and (now < today7pm) and (CRYO == 1):
 		print('White light')
 		MY_CYCLE = colorschemes.Solid(num_led=NUM_LED, pause_value=4,
                             num_steps_per_cycle=1, num_cycles=1)
 		MY_CYCLE.start()
 		CRYO = 4
 
-	if (now < today7am) and (CRYO = 3):
+	if (now < today7am) and (CRYO == 3):
 		print('Five quick trips through the rainbow')
 		MY_CYCLE = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
 							num_steps_per_cycle=35, num_cycles=5)
 		MY_CYCLE.start()
 		CRYO = 1
 
-	if (now > today7pm) and (CRYO = 4):
+	if (now > today7pm) and (CRYO == 4):
 		print('Five quick trips through the rainbow')
 		MY_CYCLE = colorschemes.TheaterChase(num_led=NUM_LED, pause_value=0.04,
 							num_steps_per_cycle=35, num_cycles=5)
