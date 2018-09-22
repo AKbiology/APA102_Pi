7de15380e0eb0dca320f745c324c36c1d147cf81
Aaron King <aaron.k.biology@gmail.com>
2018-09-22 07:48:54 -1000

    rainbow

--- rainbow.py
+++ rainbow.py
@@ -8,7 +8,7 @@ print('Repeated rainbow')
 
 while True:
 		MY_CYCLE = colorschemesmod.Rainbow(num_led=NUM_LED, pause_value=0.05,
-									   num_steps_per_cycle=255, num_cycles=5)
+									   num_steps_per_cycle=255, num_cycles=10000)
 		MY_CYCLE.start()
 
 		print('Finished a cycle')
