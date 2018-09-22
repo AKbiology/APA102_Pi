997cac5fedee63716456db5c84d28e33639ff8e8
Aaron King <aaron.k.biology@gmail.com>
2018-09-22 07:30:53 -1000

    rainbow

--- rainbow.py
+++ rainbow.py
@@ -5,7 +5,8 @@ import colorschemes
 NUM_LED = 200
 
 print('Repeated rainbow')
-While True:
+
+while True:
 		MY_CYCLE = colorschemes.Rainbow(num_led=NUM_LED, pause_value=0.05,
 									   num_steps_per_cycle=255, num_cycles=5)
 		MY_CYCLE.start()
